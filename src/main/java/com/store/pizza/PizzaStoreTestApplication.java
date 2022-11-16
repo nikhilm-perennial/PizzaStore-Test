b7ce252de23faeb316425457584edbc8a9391c7a
Nikhil Madne <nikhil.madne@perennialsys.com>
2022-11-16 09:34:20 +0530

    new commit

---
+++ src/main/java/com/store/pizza/PizzaStoreTestApplication.java
@@ -0,0 +1,13 @@
+package com.store.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaStoreTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaStoreTestApplication.class, args);
+    }
+
+}
